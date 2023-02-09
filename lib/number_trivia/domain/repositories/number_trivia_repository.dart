49456e73dc180b4c84744c7a5b30281eaab00a24
Richard <ric17101raguine@gmail.com>
2023-02-09 14:07:19 +0800

    add repository and entity files

---
+++ lib/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia_app/core/error/failures.dart';
+import 'package:number_trivia_app/number_trivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
